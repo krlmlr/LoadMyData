7986c496ba556ffb8d0963adbc510564ba0f4e07
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-09-24 11:49:25 +0200

    don't need triple-colon qualification in tests

--- tests/testthat/test-varnames.R
+++ tests/testthat/test-varnames.R
@@ -1,10 +1,10 @@
 context('Varnames')
 
 test_that('Cleans variable names', {
-  expect_that(ProjectTemplate:::clean.variable.name('test_me'), equals('test.me'))
-  expect_that(ProjectTemplate:::clean.variable.name('test-me'), equals('test.me'))
-  expect_that(ProjectTemplate:::clean.variable.name('test..me'), equals('test.me'))
-  expect_that(ProjectTemplate:::clean.variable.name('test me'), equals('test.me'))
-  expect_that(ProjectTemplate:::clean.variable.name('1990'), equals('X1990'))
+  expect_that(clean.variable.name('test_me'), equals('test.me'))
+  expect_that(clean.variable.name('test-me'), equals('test.me'))
+  expect_that(clean.variable.name('test..me'), equals('test.me'))
+  expect_that(clean.variable.name('test me'), equals('test.me'))
+  expect_that(clean.variable.name('1990'), equals('X1990'))
 })
 
