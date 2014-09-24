eeb543423ee179be7501e954227cb09908734097
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-09-24 20:24:47 +0200

    new parameter override_extension to dataformat constructor; extract only the last part of a multi-part extension such as .csv.bz2

--- R/dataformat.R
+++ R/dataformat.R
@@ -3,7 +3,9 @@
 #' Functions to help selecting the correct loader for a given file name.
 #'
 #' @param file_name Path to a file.
-#' @param check_exists Check if file exists.
+#' @param check_exists Check if file exists (default).
+#' @param override_extension Alternative file extension (default: use extension
+#'   from \code{file_name}).
 #' @param x An object.
 #'
 #' @return An object of class \code{dataformat}
@@ -12,7 +14,7 @@
 #'
 #' @examples
 #' \dontrun{dataformat("my/data/file.csv.bz2")}
-dataformat <- function(file_name, check_exists = TRUE) {
+dataformat <- function(file_name, check_exists = TRUE, override_extension = NULL) {
   if (!is.character(file_name))
     stop("file_name must be a character")
   if (length(file_name) != 1L)
@@ -20,16 +22,13 @@ dataformat <- function(file_name, check_exists = TRUE) {
   if (check_exists && !file.exists(file_name))
     stop("file ", file_name, " does not exist")
   base <- basename(file_name)
-  dotted_components <- strsplit(base, ".", fixed = TRUE)[[1]]
-  if (length(dotted_components) <= 1L)
-    stop("cannot detect extension from file ", file_name)
-  class_names <- vapply(
-    seq.int(2L, length(dotted_components)),
-    function(x)
-      paste(c("dataformat", dotted_components[seq.int(x, length(dotted_components))]),
-            collapse = "."),
-    character(1)
-  )
+  if (is.null(override_extension)) {
+    dotted_components <- strsplit(base, ".", fixed = TRUE)[[1L]]
+    if (length(dotted_components) <= 1L)
+      stop("cannot detect extension from file ", file_name)
+    override_extension <- tail(dotted_components, 1L)
+  }
+  class_names <- sprintf("dataformat.%s", override_extension)
   class_names <- c(class_names, "dataformat")
   structure(file_name, class = class_names)
 }
