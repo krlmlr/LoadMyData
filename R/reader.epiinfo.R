5d77639805ad3c055a0c2b1c7a6aa469c45b7c25
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-21 17:47:56 +0200

    epiinfo

--- R/reader.epiinfo.R
+++ R/reader.epiinfo.R
@@ -3,9 +3,7 @@
 #' This function will load all of the data sets stored in the specified
 #' Epi Info file into the global environment.
 #'
-#' @param data.file The name of the data file to be read.
 #' @param x The path to the data set to be loaded.
-#' @param variable.name The name to be assigned to in the global environment.
 #' @param ... Further arguments.
 #'
 #' @return No value is returned; this function is called for its side effects.
@@ -14,11 +12,9 @@
 #'
 #' @examples
 #' \dontrun{reader.epiinfo('example.rec', 'data/example.rec', 'example')}
-reader.dataformat.epiinfo <- function(x, data.file, variable.name, ...)
+reader.dataformat.epiinfo <- function(x, ...)
 {
   .require.package('foreign')
 
-  assign(variable.name,
-         foreign::read.epiinfo(x),
-         envir = .TargetEnv)
+  list(foreign::read.epiinfo(x))
 }
