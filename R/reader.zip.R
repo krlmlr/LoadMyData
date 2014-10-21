386651d0a13a5de54d8fe8e6827799366ff76265
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-21 17:25:34 +0200

    zip

--- R/reader.zip.R
+++ R/reader.zip.R
@@ -1,23 +1,19 @@
 #' Read a zip-compressed remote file
 #'
-#' This function will load data from a zip-compressed file.  The file names must
-#' be specified.
+#' This function will load data from a zip-compressed file.  Only the first file
+#' will be extracted.
 #'
-#' @param data.file The name of the data file to be read.
 #' @param x The path to the data set to be loaded.
-#' @param variable.name The name to be assigned to in the global environment.
 #' @param ... Further arguments passed to the reader invoked.
 #'
 #' @return No value is returned; this function is called for its side effects.
 #'
 #' @export
-reader.dataformat.zip <- function(x, data.file, variable.name, ...)
+reader.dataformat.zip <- function(x, ...)
 {
-  tmp.dir <- tempdir()
-  tmp.path <- file.path(tmp.dir, data.file)
-  file.copy(x, tmp.path)
-  unzip(x, exdir = tmp.dir)
-  x <- file.path(tmp.dir, sub('\\.zip$', '', data.file))
-
-  reader(dataformat(x), variable.name = variable.name, ...)
+  tmp.path <- tempdir()
+  oldwd <- setwd(tmp.path)
+  on.exit(setwd(oldwd), add = TRUE)
+  files <- unzip(x)
+  reader(dataformat(files[[1]]), ...)
 }
