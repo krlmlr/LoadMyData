55c9e8347b3e937384f5908f28f3b18873c0dad1
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-28 12:50:46 +0100

    unify again

--- R/reader.csv.R
+++ R/reader.csv.R
@@ -1,17 +1,9 @@
-#' Read a comma separated values (.csv) file.
+#' @details
+#' \code{dbf}: CSV file (comma-separated) via \code{\link{read.csv}}
 #'
-#' This function will load a data set stored in the CSV file format into
-#' the specified global variable binding.
-#'
-#' @param x The path to the data set to be loaded.
-#' @param ... Further arguments.
-#'
-#' @return No value is returned; this function is called for its side effects.
-#'
+#' @usage NULL
+#' @rdname reader
 #' @export
-#'
-#' @examples
-#' \dontrun{reader.csv('example.csv', 'data/example.csv', 'example')}
 reader.dataformat.csv <- function(x, ...)
 {
   read_atomic(x, .f = read.csv, header = TRUE, sep = ',')
